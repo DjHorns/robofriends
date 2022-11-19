5db24ad5f09d2e689c1aebb351e20cb4655cd9b1
DjHorns <dewaynecode123@gmail.com>
2022-11-19 15:52:43 -0600

    add deploy scripts

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -7,17 +7,18 @@ class ErrorBoundary extends React.Component {
             hasError: false
         }
     }
-}
+
 
 componentDidCatch(error, info) {
-    this.setState({ hasError: true })
+    this.setState({ hasError: true });
 }
 
 render () {
     if (this.state.hasError) {
-        return <h1>Oooops. That is not good</h1>
+        return <h1>Oooops. That is not good</h1>;
     }
-    return this.props.children 
+    return this.props.children;
 }
 
+}
 export default ErrorBoundary;
