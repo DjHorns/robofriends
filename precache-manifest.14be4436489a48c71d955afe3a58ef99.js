c33d858c6028e2fd245b5a5e1d53e7b76e4879e1
DjHorns <dewaynecode123@gmail.com>
2022-11-19 15:53:17 -0600

    Updates

---
+++ precache-manifest.14be4436489a48c71d955afe3a58ef99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "af42afd155aa1af36d4d",
+    "url": "/robofriends/static/css/main.0738526e.chunk.css"
+  },
+  {
+    "revision": "af42afd155aa1af36d4d",
+    "url": "/robofriends/static/js/main.af42afd1.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriends/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "1362b4963c55baa42545",
+    "url": "/robofriends/static/css/2.a893a1ad.chunk.css"
+  },
+  {
+    "revision": "1362b4963c55baa42545",
+    "url": "/robofriends/static/js/2.1362b496.chunk.js"
+  },
+  {
+    "revision": "6bf665704170461feaf8",
+    "url": "/robofriends/static/js/runtime~main.6bf66570.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "33b2698e5afb3135f281cc996c578011",
+    "url": "/robofriends/index.html"
+  }
+];
